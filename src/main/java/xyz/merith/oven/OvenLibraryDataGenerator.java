2d8df2581703715925b6a2158df2f9807c6bdb82
Merith-TK <merith.tk@gmail.com>
2024-07-21 17:48:21 -0700

    first commit

---
+++ src/main/java/xyz/merith/oven/OvenLibraryDataGenerator.java
@@ -0,0 +1,11 @@
+package xyz.merith.oven;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OvenLibraryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
